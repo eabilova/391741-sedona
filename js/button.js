a3d3576adc977fb7454716374d23c540efbb7a98
Elena <44971284+eabilova@users.noreply.github.com>
2019-01-06 18:07:42 +0300

    испраление ошибок по базовым критериям

--- js/button.js
+++ js/button.js
@@ -2,8 +2,7 @@ window.addEventListener('load', function () {
 
   document.getElementById("search_form").classList.add('hidden')
 
-  var button = document.getElementById("search_button")
-  if (button) {
+  var button = document.getElementById("search_button") if (button) {
     button.addEventListener('click', toggle_search_form)
   }
 
