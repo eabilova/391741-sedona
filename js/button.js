8412423c8e2b033b224da82954a2b3a1aa964318
Elena <44971284+eabilova@users.noreply.github.com>
2019-01-07 16:10:50 +0300

    правка стилей после второй проверки наставником

--- js/button.js
+++ js/button.js
@@ -2,7 +2,8 @@ window.addEventListener('load', function () {
 
   document.getElementById("search_form").classList.add('hidden')
 
-  var button = document.getElementById("search_button") if (button) {
+  var button = document.getElementById("search_button")
+  if (button) {
     button.addEventListener('click', toggle_search_form)
   }
 
